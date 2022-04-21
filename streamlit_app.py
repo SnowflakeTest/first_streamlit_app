50bd851bd9e993836430e8353c34513482d8de65
SnowflakeTest <104119973+SnowflakeTest@users.noreply.github.com>
2022-04-21 09:58:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Parents First Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.title('Idle')
+streamlit.title('Dosa')
+streamlit.title('Pongal')
+streamlit.title('Puttu')
