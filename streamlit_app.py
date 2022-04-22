ccc3f1e4f4dca1bb2363cac42ef7d9bb8bc3267e
SnowflakeTest <104119973+SnowflakeTest@users.noreply.github.com>
2022-04-22 16:47:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,7 @@ def get_fruityvice_data(this_fruit_choice):
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
 
 def insert_row_snowflake(new_fruit):
